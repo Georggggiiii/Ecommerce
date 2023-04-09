a7df9cfa849eb2e684e73058575b7507eeb65758
adorable <yle>
2023-04-09 20:48:10 +0400

    Added NotFound Page

---
+++ src/views/NotFound.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="not-found">
+        <h1>404: Page Not Found</h1>
+        <h1> <router-link to="/"><span class="continue">Continue shopping</span></router-link></h1>
+    </div>
+</template>
+
+<style scoped>
+.not-found {
+    text-align:center;
+    align-items:center;
+    margin-top:10%;
+}
+* {
+    text-decoration: none;
+}
+.continue {
+    color:black;
+    text-decoration: underline;
+}
+</style>
